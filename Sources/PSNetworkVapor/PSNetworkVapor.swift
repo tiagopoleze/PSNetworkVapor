cdb2f49f9384844f823beab67c9457dc707ee5f7
Tiago Ferreira <tiagopoleze@icloud.com>
2023-06-04 08:33:42 +0200

    Small fix

--- Sources/PSNetworkVapor/PSNetworkVapor.swift
+++ Sources/PSNetworkVapor/PSNetworkVapor.swift
@@ -17,12 +17,14 @@ public extension RoutesBuilder {
     @discardableResult
     func endpoint<T: PSRequest>(
         _ endpoint: T.Type,
-        use closure: @escaping (Vapor.Request, T.BodyParameter) async throws -> T.ResponseModel
+        use closure: @escaping (Vapor.Request, T.BodyParameter?) async throws -> T.ResponseModel
     ) -> Vapor.Route where T.ResponseModel: Vapor.AsyncResponseEncodable
     {
         return self.on(endpoint.method.nio, endpoint.path.pathComponent) { request async throws -> T.ResponseModel in
-            let content = try request.content.decode(endpoint.BodyParameter)
-            return try await closure(request, content)
+            if let content = try? request.content.decode(endpoint.BodyParameter) {
+                return try await closure(request, content)
+            }
+            return try await closure(request, nil)
         }
     }
 }
