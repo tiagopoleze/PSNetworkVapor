bac919c8b4b8cb1c87de29bf2b4ef3af297ed465
Tiago Ferreira <tiagopoleze@icloud.com>
2023-06-04 08:21:28 +0200

    Updated PSNetwork

--- Tests/PSNetworkVaporTests/PSNetworkVaporTests.swift
+++ Tests/PSNetworkVaporTests/PSNetworkVaporTests.swift
@@ -4,8 +4,37 @@ import Vapor
 @testable import PSNetworkVapor
 
 final class PSNetworkVaporTests: XCTestCase {
-
+    let app = Vapor.Application()
     func testExample() throws {
+        XCTAssertEqual(app.routes.all.count, 0)
+        GetRequest.register(with: app)
+        XCTAssertEqual(app.routes.all.count, 1)
+    }
+}
 
+extension EmptyBodyParameter: Content { }
+struct GetOutput: Content {
+    let name: String
+}
+
+extension GetOutput: AsyncResponseEncodable { }
+
+struct GetRequest: PSRequest {
+    typealias BodyParameter = EmptyBodyParameter
+    typealias ResponseModel = GetOutput
+
+    static var method: PSNetwork.Method = .get
+    static var path: [String] = []
+
+    var bodyParameter: EmptyBodyParameter? = nil
+    var authorizationType: PSNetwork.AuthorizationType = .none
+    var host: String = ""
+}
+
+extension GetRequest: Endpoint {
+    static func register(with routes: RoutesBuilder) {
+        routes.endpoint(self) { request, body in
+            .init(name: "Tiago")
+        }
     }
 }
